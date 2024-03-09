1084040ad39eac98957a3f397756fd5789719be7
Edwar Arango <edwar.arango@galileo.edu>
2024-03-08 23:09:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# proyecto_php_FCS
+aplicación en el lenguaje de programación PHP que muestre en pantalla un “Hola mundo
---
+++ hola.php
@@ -0,0 +1,8 @@
+<?php
+    // $variable = 5 + 6;
+    // echo "Hola dedsde php ". $variable;
+
+    echo "<h1>Hola mundo, Soy Edwar Arango</h1>"
+?>
+
+<img src="Honda_Civic_Ex.jpg" alt="Honda Civic Ex" width="500" heigth="500">
